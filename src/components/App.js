4939c7c1ae3539a46ede4b4f4a1b07a70d3088a7
Sandeep Pagi <nodejs.pagi@team.telstra.com>
2019-11-12 22:03:58 +0530

    XML to JSON

--- src/components/App.js
+++ src/components/App.js
@@ -2,8 +2,54 @@ import React from "react";
 // import PostList from './PostList';
 import CompareButton from "./CompareButton";
 import InputFields from "./InputFields";
+import XMLParser from "react-xml-parser";
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      error: null,
+      isLoaded: false,
+      items: []
+    };
+  }
+
+  componentDidMount() {
+    fetch(
+      " http://cul.esla.internal.services.rccl.com/esl/search/rest/cruiseSearch?header.application=royalcaribbean.com&header.brand=R&header.domainId=1&header.language=en_US&criteria.office=LON&criteria.country=GBR&criteria.currency=GBP&criteria.channel=VP&criteria.bookingType=FIT&criteria.cruiseType.value=CO,CT&criteria.brand.value=R&qualifiers.priceScope=REQUESTED_AVAILABLE&qualifiers.searchScope=ALL&resultsPreference.groupBy=PACKAGE&resultsPreference.includeFacets=false&resultsPreference.includePrices=true&resultsPreference.includeResults=true&resultsPreference.pagination.count=10&resultsPreference.pagination.offset=0&resultsPreference.priceLevel.bestPricedCategory=true&resultsPreference.sortBy=PROMOTIONAL_DEALS&resultsPreference.sortOrder=ASCENDING&resultsPreference.strictSearch=true&criteria.packageId.value=AL07W294&criteria.sailingDate.date=2019-12-01",
+      {
+        method: "GET", // *GET, POST, PUT, DELETE, etc.
+        headers: {
+          Accept: "application/xml"
+          // Accept: "application/json"
+        }
+      }
+    )
+      .then(res => res.text())
+      .then(result => {
+        console.log("result --", result);
+        var xml = new XMLParser().parseFromString(result);
+        console.log(xml);
+        // console.log(xml.getElementsByTagName("host")[0].value);
+      });
+    // .then(
+    //   result => {
+    //     this.setState({
+    //       isLoaded: true,
+    //       items: result.items
+    //     });
+    //   },
+    //   // Note: it's important to handle errors here
+    //   // instead of a catch() block so that we don't swallow
+    //   // exceptions from actual bugs in components.
+    //   error => {
+    //     this.setState({
+    //       isLoaded: true,
+    //       error
+    //     });
+    //   }
+    // );
+  }
   render() {
     return (
       <div className="ui container">

26579b9f11b132f6a9fee7122ea845ce65f83660
Sandeep Pagi <nodejs.pagi@team.telstra.com>
2019-11-15 21:55:54 +0530

    bug fix

--- src/components/App.js
+++ src/components/App.js
@@ -12,26 +12,6 @@ class App extends React.Component {
     };
   }
 
-  componentDidMount() {
-    fetch(
-      //"http://mockbin.org/request
-      " http://cul.esla.internal.services.rccl.com/esl/search/rest/cruiseSearch?header.application=royalcaribbean.com&header.brand=R&header.domainId=1&header.language=en_US&criteria.office=LON&criteria.country=GBR&criteria.currency=GBP&criteria.channel=VP&criteria.bookingType=FIT&criteria.cruiseType.value=CO,CT&criteria.brand.value=R&qualifiers.priceScope=REQUESTED_AVAILABLE&qualifiers.searchScope=ALL&resultsPreference.groupBy=PACKAGE&resultsPreference.includeFacets=false&resultsPreference.includePrices=true&resultsPreference.includeResults=true&resultsPreference.pagination.count=10&resultsPreference.pagination.offset=0&resultsPreference.priceLevel.bestPricedCategory=true&resultsPreference.sortBy=PROMOTIONAL_DEALS&resultsPreference.sortOrder=ASCENDING&resultsPreference.strictSearch=true&criteria.packageId.value=AL07W294&criteria.sailingDate.date=2019-12-01",
-      {
-        method: "GET", // *GET, POST, PUT, DELETE, etc.
-        headers: {
-          //Accept: "application/xml"
-          Accept: "application/json"
-        }
-      }
-    )
-      .then(res => res.text())
-      .then(result => {
-        console.log("result --", result);
-        var xml = new XMLParser().parseFromString(result);
-        console.log(xml);
-        // console.log(xml.getElementsByTagName("host")[0].value);
-      });
-  }
   render() {
     return (
       <div className="ui container">
